af035352df612609bfeaa9d4172ec610e43e1bbf
Marius Lindegård <>
2021-05-31 07:19:01 +0000

    Basic web server setup

--- main.py
+++ main.py
@@ -1,27 +1,33 @@
-# Flask test server
-import datetime
-from termcolor import colored
-from flask import Flask
+import random, string
+from flask import Flask, render_template
 
-app = Flask(__name__)
-#This need to stay on top of the code after the imports
-print(colored('----STARTING SERVER----', 'green'))
-startup_time = datetime.datetime.now()
-print(startup_time.strftime("Time: %H:%M:%S"))
-print()
-#-----------------------------------------
+app = Flask(  # Create a flask app
+	__name__,
+	template_folder='templates',  # Name of html file folder
+	static_folder='static'  # Name of directory for static files
+)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+ok_chars = string.ascii_letters + string.digits
 
-print(colored('----SERVER HAS STARTED----', 'green'))
-bot_ready_time = datetime.datetime.now()
-print(bot_ready_time.strftime("Time: %H:%M:%S"))
-time = bot_ready_time - startup_time
-if time > datetime.timedelta(seconds=10):
-    color = "red"
-else:
-    color = "green"
 
-print(colored("Start up time:", "green"), colored(time, color))
+@app.route('/')  # What happens when the user visits the site
+def base_page():
+	random_num = random.randint(1, 100000)  # Sets the random number
+	return render_template(
+		'base.html',  # Template file path, starting from the templates folder. 
+		random_number=random_num  # Sets the variable random_number in the template
+	)
+
+
+@app.route('/2')
+def page_2():
+	rand_ammnt = random.randint(10, 100)
+	random_str = ''.join(random.choice(ok_chars) for a in range(rand_ammnt))
+	return render_template('site_2.html', random_str=random_str)
+
+
+if __name__ == "__main__":  # Makes sure this is the main process
+	app.run( # Starts the site
+		host='0.0.0.0',  # Establishes the host, required for repl to detect the site
+		port=random.randint(2000, 9000)  # Randomly select the port the machine hosts on.
+	)
